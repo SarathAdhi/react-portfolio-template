abb59c05f528e381195b2f9059f425550bee7c80
SarathAdhi <s.sarathadhithya@gmail.com>
2024-09-30 18:08:29 +0530

    feat: seo optimization

---
+++ src/app/sitemap.ts
@@ -0,0 +1,16 @@
+import { MetadataRoute } from "next";
+
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  return [
+    {
+      url: `${baseUrl}`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/studio`,
+      lastModified: new Date(),
+    },
+  ];
+}
